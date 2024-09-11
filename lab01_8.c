7585bb4760963b7e333f3879b299eb095e13ce07
Sachidananda09 <i24ai025@aid.svnit.ac.in>
2024-09-11 10:00:13 +0530

    Update lab01_8.c

--- lab01_8.c
+++ lab01_8.c
@@ -1,16 +1,17 @@
+//time period coversion
 #include <stdio.h>
 
 int main(){
-    int time_period_of_revolution, days, hours, minutes ,seconds;
+    int revolution, days, hours, minutes ,seconds;
     time_period_of_revolution=31558150;
-    days = time_period_of_revolution / 86400;
-    time_period_of_revolution = time_period_of_revolution % 86400;
+    days = revolution / 86400;
+    revolution = revolution % 86400;
 
     hours = time_period_of_revolution / 3600;
-    time_period_of_revolution = time_period_of_revolution% 3600;
+    revolution = revolution% 3600;
 
-     minutes = time_period_of_revolution / 60;
-     seconds = time_period_of_revolution % 60;
+     minutes = revolution / 60;
+     seconds = revolution % 60;
 
     printf("Days:%d, hours:%d, minutes%d, seconds:%d",days, hours ,minutes, seconds);
     return 0;
