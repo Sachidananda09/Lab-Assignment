f75ea8e869ab4c925a8515e6c6d3ab6efa78f801
Sachidananda09 <i24ai025@aid.svnit.ac.in>
2024-09-11 09:53:15 +0530

    Update lab01_3.c

--- lab01_3.c
+++ lab01_3.c
@@ -1,14 +1,14 @@
+//3. programe to calculate gross salary
 #include <stdio.h>
 
 int main(){
-    float basic_salary, hra,DA,Transport_allowance,Medical_allowance,Gross_salary;
+    float basic_salary, hra,Transport_allowance,Medical_allowance,Gross_salary;
     printf("Enter your basic salary:\n");
     scanf("%f",&basic_salary);
-    hra= 0.20*basic_salary;
-    Transport_allowance= 0.10*basic_salary;
-    Medical_allowance= 5000;
-    DA= 3000;
-    Gross_salary=hra+DA+Transport_allowance+basic_salary+Medical_allowance;
+    hra= 0.30*basic_salary;
+    Transport_allowance= 0.20*basic_salary;
+    Medical_allowance= 4000;
+    Gross_salary=hra+Transport_allowance+basic_salary+Medical_allowance;
     printf("Your Gross salary is %d\n",Gross_salary);
     return 0;
 }
